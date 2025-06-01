5243522995170dda027a2ae94e327475c0e2b1f2
Ivan <ivan.grgec123grgec@gmail.com>
2025-06-01 14:10:11 +0200

    Update workspace.rs

--- src/ui/workspace.rs
+++ src/ui/workspace.rs
@@ -2,6 +2,6 @@ use eframe::egui::{self, CentralPanel, Context};
 
 pub fn draw_workspace(ctx: &Context) {
     CentralPanel::default().show(ctx, |_ui| {
-        // reserved for virtual desktop, wallpaper, etc.
+        // TODO - virtual desktop, wallpaper, etc.
     });
 }
